fb7bb272640216d91220c9a8c5a33a58caa65a53
Chrission <christianaraymon@gmail.com>
2025-02-08 19:04:56 -0800

    basic map functionality with page routing

--- storkdork/StorkDorkMain/Controllers/HomeController.cs
+++ storkdork/StorkDorkMain/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Map()
+    {
+        return View();
+    }
+
     public IActionResult Error()
     {
         var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
