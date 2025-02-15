59d3148de2029a40c7a01b8d67ebedcad408ba08
Hunter <113154751+HunterWinger@users.noreply.github.com>
2025-02-14 17:39:06 -0800

    Scaffolded the identity default ui pages

--- storkdork/StorkDorkMain/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ storkdork/StorkDorkMain/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -68,7 +68,6 @@ namespace StorkDork.Areas.Identity.Pages.Account
             [EmailAddress]
             public string Email { get; set; }
 
-
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -129,7 +128,7 @@ namespace StorkDork.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, $"Invalid login attempt.{Input.Password}");
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                     return Page();
                 }
             }
--- storkdork/StorkDorkMain/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ storkdork/StorkDorkMain/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,8 +18,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
-using StorkDorkMain.Models;
-using StorkDorkMain.Data;
 
 namespace StorkDork.Areas.Identity.Pages.Account
 {
@@ -31,15 +29,13 @@ namespace StorkDork.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
-        private readonly StorkDorkContext _context;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender,
-            StorkDorkContext context)
+            IEmailSender emailSender)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -47,7 +43,6 @@ namespace StorkDork.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
-            _context = context;
         }
 
         /// <summary>
@@ -85,22 +80,6 @@ namespace StorkDork.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             /// <summary>
-            ///  Added support for our stork dork side First Name
-            /// </summary>
-            [Required]
-            [StringLength(50)]
-            [Display(Name = "FirstName")]
-            public string FirstName { get; set; }
-
-            /// <summary
-            /// Added support for our stork dork side Last Name
-            /// </summary>
-            [Required]
-            [StringLength(50)]
-            [Display(Name = "FirstName")]
-            public string LastName { get; set; }
-
-            /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
@@ -131,72 +110,49 @@ namespace StorkDork.Areas.Identity.Pages.Account
         {
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                var result = await _userManager.CreateAsync(user, Input.Password);
 
-                try
+                if (result.Succeeded)
                 {
-                    var result = await _userManager.CreateAsync(user, Input.Password);
+                    _logger.LogInformation("User created a new account with password.");
 
-                    if (result.Succeeded)
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        _logger.LogInformation("User created a new account with password.");
-
-                        var sdUser = new SdUser
-                        {
-                            AspNetIdentityId = user.Id, // Associate with the Identity user
-                            FirstName = Input.FirstName,
-                            LastName = Input.LastName
-                        };
-
-                        _context.SdUsers.Add(sdUser);
-                        await _context.SaveChangesAsync();
-
-                        var userId = await _userManager.GetUserIdAsync(user);
-                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                        var callbackUrl = Url.Page(
-                            "/Account/ConfirmEmail",
-                            pageHandler: null,
-                            values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                            protocol: Request.Scheme);
-
-                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                        if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                        {
-                            return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                        }
-                        else
-                        {
-                            await _signInManager.SignInAsync(user, isPersistent: false);
-                            TempData["SuccessMessage"] = "You have successfully registered and logged in!";
-                            return LocalRedirect(returnUrl);
-                        }
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
                     }
-
-                    foreach (var error in result.Errors)
+                    else
                     {
-                        ModelState.AddModelError(string.Empty, error.Description);
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
                     }
                 }
-                catch (Exception ex)
+                foreach (var error in result.Errors)
                 {
-                    _logger.LogError(ex, "An error occurred while creating a new user.");
-                    ModelState.AddModelError(string.Empty, "An unexpected error occurred. Please try again later.");
+                    ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
 
-            // If we got this far, something failed, redisplay form with error message
+            // If we got this far, something failed, redisplay form
             return Page();
         }
-
 
         private IdentityUser CreateUser()
         {
