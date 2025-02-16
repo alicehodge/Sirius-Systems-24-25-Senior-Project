a95e155f441044afe0a77a597aec19480f3ea0ae
alicehodge <59610552+alicehodge@users.noreply.github.com>
2025-02-15 17:23:24 -0800

    feat: add search preview and result pagination

---
+++ storkdork/StorkDorkMain/Models/BirdPreview.cs
@@ -0,0 +1,9 @@
+namespace StorkDorkMain.Models
+{
+    public class BirdPreview
+    {
+        public int Id { get; set; }
+        public string CommonName { get; set; }
+        public string ScientificName { get; set; }
+    }
+}
---
+++ storkdork/StorkDorkMain/ViewModels/SearchResultsViewModel.cs
@@ -0,0 +1,10 @@
+namespace StorkDorkMain.Models
+{
+    public class SearchResultsViewModel
+    {
+        public IEnumerable<Bird> Birds { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string SearchTerm { get; set; }
+    }
+}
