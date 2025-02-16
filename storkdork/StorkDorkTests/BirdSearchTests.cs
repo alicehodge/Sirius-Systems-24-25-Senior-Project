e0c4f49a5f455bf1d5b88c9ed2c5b4699efe91a1
alicehodge <59610552+alicehodge@users.noreply.github.com>
2025-02-16 02:39:03 -0800

    feat: add bird search by taxonomix info
    adds the option to search by taxonomic name instead of info, fixes some naming mistakes, also adds sln file for parent storkdork directory for testing

--- storkdork/StorkDorkTests/BirdSearchTests.cs
+++ storkdork/StorkDorkTests/BirdSearchTests.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using StorkDork.Controllers;
+using StorkDorkMain.Controllers;
 using StorkDorkMain.DAL.Abstract;
 using StorkDorkMain.Models;
 using NUnit.Framework;
@@ -23,9 +23,30 @@ namespace StorkDorkTests
             
             _testBirds = new List<Bird>
             {
-                new Bird { Id = 1, CommonName = "American Robin", ScientificName = "Turdus migratorius" },
-                new Bird { Id = 2, CommonName = "Blue Jay", ScientificName = "Cyanocitta cristata" },
-                new Bird { Id = 3, CommonName = "Northern Cardinal", ScientificName = "Cardinalis cardinalis" }
+                new Bird { 
+                    Id = 1, 
+                    CommonName = "American Robin", 
+                    ScientificName = "Turdus migratorius",
+                    Order = "Passeriformes",
+                    FamilyCommonName = "Thrushes",
+                    FamilyScientificName = "Turdidae"
+                },
+                new Bird { 
+                    Id = 2, 
+                    CommonName = "Blue Jay", 
+                    ScientificName = "Cyanocitta cristata",
+                    Order = "Passeriformes",
+                    FamilyCommonName = "Jays and Crows",
+                    FamilyScientificName = "Corvidae"
+                },
+                new Bird { 
+                    Id = 3, 
+                    CommonName = "Northern Cardinal", 
+                    ScientificName = "Cardinalis cardinalis",
+                    Order = "Passeriformes",
+                    FamilyCommonName = "Cardinals",
+                    FamilyScientificName = "Cardinalidae"
+                }
             };
         }
 
@@ -49,6 +70,7 @@ namespace StorkDorkTests
         {
             // Arrange
             var searchTerm = "robin";
+            var searchType = "name";
             _mockBirdRepo.Setup(repo => repo.GetBirdsByName(searchTerm))
                         .ReturnsAsync(_testBirds.Where(b => 
                             b.CommonName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) || 
@@ -56,15 +78,47 @@ namespace StorkDorkTests
                         .ToList());
 
             // Act
-            var result = await _controller.SearchBirds(searchTerm);
+            var result = await _controller.SearchBirds(searchTerm, searchType);
 
             // Assert
             Assert.That(result, Is.TypeOf<ViewResult>());
             var viewResult = (ViewResult)result;
             Assert.That(viewResult.Model, Is.TypeOf<SearchResultsViewModel>());
             var model = (SearchResultsViewModel)viewResult.Model;
-            Assert.That(model.Birds.Count(), Is.EqualTo(1));
-            Assert.That(model.Birds.First().CommonName, Is.EqualTo("American Robin"));
+            Assert.Multiple(() =>
+            {
+                Assert.That(model.Birds.Count(), Is.EqualTo(1));
+                Assert.That(model.Birds.First().CommonName, Is.EqualTo("American Robin"));
+                Assert.That(model.SearchType, Is.EqualTo("name"));
+            });
+        }
+
+        [Test]
+        public async Task SearchBirds_TaxonomySearch_ReturnsBirds()
+        {
+            // Arrange
+            var searchTerm = "Passeriformes";
+            var searchType = "taxonomy";
+            _mockBirdRepo.Setup(repo => repo.GetBirdsByTaxonomy(searchTerm))
+                        .ReturnsAsync(_testBirds.Where(b => 
+                            b.Order.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) || 
+                            b.FamilyCommonName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                            b.FamilyScientificName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                        .ToList());
+
+            // Act
+            var result = await _controller.SearchBirds(searchTerm, searchType);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+            var viewResult = (ViewResult)result;
+            Assert.That(viewResult.Model, Is.TypeOf<SearchResultsViewModel>());
+            var model = (SearchResultsViewModel)viewResult.Model;
+            Assert.Multiple(() =>
+            {
+                Assert.That(model.SearchType, Is.EqualTo("taxonomy"));
+                Assert.That(model.Birds, Is.Not.Empty);
+            });
         }
 
         [Test]
@@ -72,9 +126,10 @@ namespace StorkDorkTests
         {
             // Arrange
             var searchTerm = "";
+            var searchType = "name";
 
             // Act
-            var result = await _controller.SearchPreview(searchTerm);
+            var result = await _controller.SearchPreview(searchTerm, searchType);
 
             // Assert
             Assert.That(result, Is.TypeOf<JsonResult>());
@@ -113,26 +168,114 @@ namespace StorkDorkTests
         }
 
         [Test]
+        public async Task SearchPreview_ValidNameSearch_ReturnsPreviewList()
+        {
+            // Arrange
+            var searchTerm = "cardinal";
+            var searchType = "name";
+            _mockBirdRepo.Setup(repo => repo.GetBirdsByName(searchTerm))
+                        .ReturnsAsync(_testBirds.Where(b => 
+                            b.CommonName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) || 
+                            b.ScientificName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                        .ToList());
+
+            // Act
+            var result = await _controller.SearchPreview(searchTerm, searchType);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<JsonResult>());
+            var jsonResult = (JsonResult)result;
+            var birds = ((IEnumerable<BirdPreview>)jsonResult.Value).ToList();
+            
+            Assert.Multiple(() =>
+            {
+                Assert.That(birds.Count, Is.EqualTo(1));
+                var firstBird = birds.First();
+                Assert.That(firstBird.CommonName, Is.EqualTo("Northern Cardinal"));
+                Assert.That(firstBird.ScientificName, Is.EqualTo("Cardinalis cardinalis"));
+                Assert.That(firstBird.Id, Is.EqualTo(3));
+            });
+        }
+
+        [Test]
+        public async Task SearchPreview_ValidTaxonomySearch_ReturnsPreviewList()
+        {
+            // Arrange
+            var searchTerm = "Passeriformes";
+            var searchType = "taxonomy";
+            _mockBirdRepo.Setup(repo => repo.GetBirdsByTaxonomy(searchTerm))
+                        .ReturnsAsync(_testBirds.Where(b => 
+                            b.Order.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) || 
+                            b.FamilyCommonName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                            b.FamilyScientificName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                        .ToList());
+
+            // Act
+            var result = await _controller.SearchPreview(searchTerm, searchType);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<JsonResult>());
+            var jsonResult = (JsonResult)result;
+            var birds = ((IEnumerable<BirdPreview>)jsonResult.Value).ToList();
+            Assert.Multiple(() =>
+            {
+                Assert.That(birds, Is.Not.Empty);
+                var firstBird = birds.First();
+                Assert.That(firstBird.Order, Is.EqualTo("Passeriformes"));
+            });
+        }
+
+        [Test]
         public async Task SearchBirds_Pagination_ReturnsCorrectPage()
         {
             // Arrange
             var searchTerm = "bird";
+            var searchType = "name";
             var page = 2;
             _mockBirdRepo.Setup(repo => repo.GetBirdsByName(searchTerm))
                         .ReturnsAsync(_testBirds);
 
             // Act
-            var result = await _controller.SearchBirds(searchTerm, page);
+            var result = await _controller.SearchBirds(searchTerm, searchType, page);
 
             // Assert
             Assert.That(result, Is.TypeOf<ViewResult>());
             var viewResult = (ViewResult)result;
             Assert.That(viewResult.Model, Is.TypeOf<SearchResultsViewModel>());
             var model = (SearchResultsViewModel)viewResult.Model;
-            Assert.That(model.CurrentPage, Is.EqualTo(page));
+            Assert.Multiple(() =>
+            {
+                Assert.That(model.CurrentPage, Is.EqualTo(page));
+                Assert.That(model.SearchType, Is.EqualTo("name"));
+            });
         }
 
-                public void Dispose()
+        [Test]
+        public async Task SearchBirds_TaxonomyPagination_ReturnsCorrectPage()
+        {
+            // Arrange
+            var searchTerm = "Passeriformes";
+            var searchType = "taxonomy";
+            var page = 2;
+            _mockBirdRepo.Setup(repo => repo.GetBirdsByTaxonomy(searchTerm))
+                        .ReturnsAsync(_testBirds);
+
+            // Act
+            var result = await _controller.SearchBirds(searchTerm, searchType, page);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<ViewResult>());
+            var viewResult = (ViewResult)result;
+            Assert.That(viewResult.Model, Is.TypeOf<SearchResultsViewModel>());
+            var model = (SearchResultsViewModel)viewResult.Model;
+            Assert.Multiple(() =>
+            {
+                Assert.That(model.CurrentPage, Is.EqualTo(page));
+                Assert.That(model.SearchType, Is.EqualTo("taxonomy"));
+            });
+        }
+
+        public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
